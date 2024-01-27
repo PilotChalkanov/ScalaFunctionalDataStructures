2c88eb43458a37d78f02ca81c73c4816d4d8309e
PilotChalkanov <nik.chalkanov@gmail.com>
2024-01-27 17:15:19 +0200

    fix Option companion obj

--- src/main/scala/Option.scala
+++ src/main/scala/Option.scala
@@ -11,14 +11,14 @@ enum Option[+A]:
       case Some(v) => Some(f(v))
 
   def flatMap[B](f: A => Option[B]): Option[B] =
-    map(f).getOrElse(None)
-
+    this match
+      case None => None
+      case Some(a) => f(a)
 
   def getOrElse[B >: A](default: => B): B =
     this match
       case None => default
       case Some(v) => v
-
 
   def orElse[B >: A](ob: => Option[B]): Option[B] =
     map(Some(_)).getOrElse(ob)
@@ -30,8 +30,12 @@ enum Option[+A]:
       else None
     )
 
+object Option:
   def lift[A, B](f: A => B): Option[A] => Option[B] =
     _.map(f)
+
+  def map2[A, B, C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] =
+    a.flatMap(aa => b.map(bb => f(aa, bb)))
 
   def sequence[A](as: List[Option[A]]): Option[List[A]] =
     @tailrec
@@ -46,3 +50,17 @@ enum Option[+A]:
     list match
       case List() => None
       case _ => Some(list.reverse)
+
+  def sequence_1[A](as: List[Option[A]]): Option[List[A]] =
+    as.foldRight[Option[List[A]]](Some(Nil))((a, acc) => map2(a, acc)(_ :: _))
+
+  def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] =
+    a match
+      case Nil => Some(Nil)
+      case h :: t => map2(f(h), traverse(t)(f))(_ :: _)
+
+  def traverse_1[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] =
+    a.foldRight[Option[List[B]]](Some(Nil))((a,acc) => map2(f(a), acc)(_ :: _))
+
+  def sequenceViaTraverse[A](a: List[Option[A]]): Option[List[A]] =
+    traverse(a)(x => x)
