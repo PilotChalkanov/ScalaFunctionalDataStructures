182e09b329a10fe389e4e4b2d7dd2ff663bc2a67
PilotChalkanov <nik.chalkanov@gmail.com>
2024-01-28 13:25:21 +0200

    implement Validated as Enum

--- src/main/scala/Either.scala
+++ src/main/scala/Either.scala
@@ -1,5 +1,6 @@
 import scala.annotation.tailrec
 import scala.util.Either
+import scala.util.control.NonFatal
 
 enum Either[+E, +A]:
   case Left(value: E)
@@ -27,10 +28,16 @@ enum Either[+E, +A]:
       b <- that
     yield f(a, b)
 
-
-  def traverseEither[E, A, B](as: List[A])(f: A => Either[E, B]): Either[E, List[B]] =
-    as.foldRight[Either[E, List[B]]](Right(Nil))((a: A, b: Either[E, List[B]]) => f(a).map2(b)(_ :: _))
-
+  def map2Both[E, A, B, C](
+                            a: Either[E, A],
+                            b: Either[E, B],
+                            f: (A, B) => C
+                          ): Either[List[E], C] =
+    (a, b) match
+      case (Right(aa), Right(bb)) => Right(f(aa, bb))
+      case (Left(e), Right(_)) => Left(List(e))
+      case (Right(_), Left(e)) => Left(List(e))
+      case (Left(e1), Left(e2)) => Left(List(e1, e2))
 
   def map2All[E, A, B, C](
                            a: Either[List[E], A],
@@ -44,3 +51,29 @@ enum Either[+E, +A]:
       case (Left(es1), Left(es2)) => Left(es1 ++ es2)
 
 
+
+object Either:
+  def mean(xs: IndexedSeq[Double]): Either[String, Double] =
+    if xs.isEmpty then
+      Left("mean of empty list!")
+    else
+      Right(xs.sum / xs.length)
+
+  def safeDiv(x: Int, y: Int): Either[Throwable, Int] =
+    try Right(x / y)
+    catch case NonFatal(t) => Left(t)
+
+  def catchNonFatal[A](a: => A): Either[Throwable, A] =
+    try Right(a)
+    catch case NonFatal(t) => Left(t)
+
+  def traverse[E, A, B](as: List[A])(f: A => Either[E, B]): Either[E, List[B]] =
+    as match
+      case Nil => Right(Nil)
+      case h :: t => (f(h).map2(traverse(t)(f)))(_ :: _)
+
+  def traverse_1[E, A, B](as: List[A])(f: A => Either[E, B]): Either[E, List[B]] =
+    as.foldRight[Either[E, List[B]]](Right(Nil))((a, b) => f(a).map2(b)(_ :: _))
+
+  def sequence[E, A](es: List[Either[E, A]]): Either[E, List[A]] =
+    traverse(es)(x => x)
---
+++ src/main/scala/Validated.scala
@@ -0,0 +1,39 @@
+enum Validated[+E, +A]:
+  case Valid(get: A)
+  case Invalid(error: E)
+
+  def toEither: Either[E, A] =
+    this match
+      case Valid(a) => Either.Right(a)
+      case Invalid(e) => Either.Left(e)
+
+  def map[B](f: A => B): Validated[E, B] =
+    this match
+      case Valid(a) => Valid(f(a))
+      case Invalid(es) => Invalid(es)
+
+  def map2[EE >: E, B, C](b: Validated[EE, B])
+                         (f: (A, B) => C)
+                         (combineErrors: (EE, EE) => EE)
+  : Validated[EE, C] =
+    (this, b) match
+      case (Valid(aa), Valid(bb)) => Valid(f(aa, bb))
+      case (Invalid(e), Valid(_)) => Invalid(e)
+      case (Valid(_), Invalid(e)) => Invalid(e)
+      case (Invalid(e1), Invalid(e2)) => Invalid(combineErrors(e1, e2))
+
+object Validated:
+  def fromEither[E, A](e: Either[E, A]): Validated[E, A] =
+    e match
+      case Either.Right(a) => Valid(a)
+      case Either.Left(e) => Invalid(e)
+
+  def traverse[E, A, B](
+                         as: List[A], f: A => Validated[E, B],
+                         combineErrors: (E, E) => E
+                       ): Validated[E, List[B]] =
+    as.foldRight(Valid(Nil): Validated[E, List[B]])((a, acc) =>
+      f(a).map2(acc)(_ :: _)(combineErrors))
+
+  def sequence[E, A](vs: List[Validated[E, A]], combineErrors: (E, E) => E): Validated[E, List[A]] =
+    traverse(vs, identity, combineErrors)
